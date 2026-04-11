558984b54a58b5a8bb0f35f9b980fab70358b5a8
Wilmerx <65931869+Wilmerx@users.noreply.github.com>
2026-04-11 00:38:24 +0000

    Actualizar carrito.js y servicios.html

--- js/carrito.js
+++ js/carrito.js
@@ -1,22 +1,22 @@
 const servicios = [
     {
-        id: 1, nombre: "LZM232", precio: 1, img: "img/LZM232.jpeg", descripcion: "Camión de carga ligera con capacidad de 5 toneladas"},
+        id: 1, nombre: "LZM232", precio: 45000, img: "img/LZM232.jpeg", descripcion: "Camión de carga ligera con capacidad de 5 toneladas"},
     {
-        id: 2, nombre: "SPM609", precio: 1000, img: "img/SPM609.jpeg", descripcion: "Camión de carga mediana con capacidad de 8 toneladas"},
+        id: 2, nombre: "SPM609", precio: 75000, img: "img/SPM609.jpeg", descripcion: "Camión de carga mediana con capacidad de 8 toneladas"},
     {
-        id: 3, nombre: "SPN970", precio: 1000, img: "img/SPN970.jpeg", descripcion: "Camión de carga pesada con capacidad de 12 toneladas"},
+        id: 3, nombre: "SPN970", precio: 75000, img: "img/SPN970.jpeg", descripcion: "Camión de carga pesada con capacidad de 12 toneladas"},
     {
-        id: 4, nombre: "SZU878", precio: 1000, img: "img/SZU878.jpeg", descripcion: "Camión refrigerado para carga perecedera"},
+        id: 4, nombre: "SZU878", precio: 45000, img: "img/SZU878.jpeg", descripcion: "Camión refrigerado para carga perecedera"},
     {
-        id: 5, nombre: "TDS155", precio: 1000, img: "img/TDS155.jpeg", descripcion: "Camión de plataforma para equipos especiales"},
+        id: 5, nombre: "TDS155", precio: 75000, img: "img/TDS155.jpeg", descripcion: "Camión de plataforma para equipos especiales"},
     {
-        id: 6, nombre: "TZS937", precio: 1000, img: "img/TZS937.jpeg", descripcion: "Camión tanque para transporte de líquidos"},
+        id: 6, nombre: "TZS937", precio: 75000, img: "img/TZS937.jpeg", descripcion: "Camión tanque para transporte de líquidos"},
     {
-        id: 7, nombre: "UFY540", precio: 1000, img: "img/UFY540.jpeg", descripcion: "Camión de transporte de autopartes"},
+        id: 7, nombre: "UFY540", precio: 75000, img: "img/UFY540.jpeg", descripcion: "Camión de transporte de autopartes"},
     {
-        id: 8, nombre: "VEO798", precio: 1000, img: "img/VEO798.jpeg", descripcion: "Camión de mudanza con equipo de carga"},
+        id: 8, nombre: "VEO798", precio: 75000, img: "img/VEO798.jpeg", descripcion: "Camión de mudanza con equipo de carga"},
     {
-        id: 9, nombre: "JTS452", precio: 1000, img: "img/JTS452.jpeg", descripcion: "Camión especializado para carga granel"}
+        id: 9, nombre: "JTS452", precio: 45000, img: "img/JTS452.jpeg", descripcion: "Capacidad de "}
 ];
 
 function agregarAlCarrito(id) {
